86fe10fbf534bb0bff8d92cf2bcf02fd622118bd
diuguide <everett.diuguid@gmail.com>
2021-03-27 13:54:16 -0400

    setting up production enviroment

--- database/index.js
+++ database/index.js
@@ -1,12 +1,18 @@
 const Pool = require('pg').Pool;
 require("dotenv").config();
 
-const pool = new Pool({
+const devConfig = {
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     host: 'localhost',
     port: 5432,
     database: process.env.DATABASE
-})
+}
+
+const proConfig = {
+    connectionString: process.env.DATABASE_URL
+}
+
+const pool = new Pool(process.env.NODE_ENV === 'production' ? proConfig : devConfig);
 
 module.exports = pool;
