82a57b1b9b03a8301788fdab50434fd081007038
diuguide <everett.diuguid@gmail.com>
2021-03-27 12:20:30 -0400

    added get, delete, get by username, routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../../database");
 
+//Create New user
 router.post("/user", async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -9,10 +10,40 @@ router.post("/user", async (req, res) => {
       `INSERT INTO users (username, password) VALUES($1, $2)`,
       [username, password]
     );
-    res.json(req.body);
-  } catch (error) {
+    res.json(newUser);
+  } catch (err) {
     console.error(err.message);
   }
+});
+
+//Get all users
+router.get("/", async (req, res) => {
+  try {
+    const allUsers = await pool.query(`SELECT * FROM users`);
+    res.json(allUsers);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//Get user by username
+router.get('/:username', async (req, res) => {
+    try {
+        const user = await pool.query(`SELECT * FROM users WHERE username=${req.params.username}`);
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+//Delete user
+router.delete("/delete/:id", async (req, res) => {
+    try {
+        const deletedUser = await pool.query(`DELETE FROM users WHERE user_id=${req.params.id}`)
+        res.json(deletedUser);
+    } catch (err) {
+        console.error(err.message)
+    }
 });
 
 module.exports = router;
