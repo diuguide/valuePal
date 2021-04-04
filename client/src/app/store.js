a1063509058a363cefa5fbbf317c2f5fe4abfddc
diuguide <everett.diuguid@gmail.com>
2021-04-03 21:48:18 -0400

    data reducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../features/auth/authSlice';
-import errorReducer from '../features/error/errorSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import errorReducer from "../features/error/errorSlice";
+import dataReducer from "../features/stockData/dataSlice";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
-    error: errorReducer
+    error: errorReducer,
+    stockData: dataReducer,
   },
 });
