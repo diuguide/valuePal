1837e31f6531f0908b8df601a09da55d7549fbb5
diuguide <everett.diuguid@gmail.com>
2021-04-27 23:53:52 -0400

    removed auth requirement to show stock ticker

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -2,7 +2,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import NavBar from "./NavBar";
 import StockQuote from "./Info/Quote";
 import StockInfo from "./Info/Info";
-import ControlPanel from "./ControlPanel";
 import WatchList from "./WatchList";
 import Welcome from "./welcome";
 import { useSelector } from "react-redux";
@@ -11,46 +10,31 @@ import ChartComponent from "./Charts";
 
 const Main = () => {
   const auth = useSelector(authState);
-  if (auth.isAuthenticated) {
-    return (
-      <>
-        <NavBar />
-        <Container fluid>
-          <Row className="bg-secondary d-flex justify-content-center">
-            <Col lg={8}>
-              <Row>
-                <Col lg={6}>
-                  <StockQuote />
-                  <ChartComponent />
-                </Col>
-                <Col lg={6}>
-                  <StockInfo />
-                  <WatchList />
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-          <Row className="bg-secondary d-flex justify-content-center">
-            <Col>
-              <Welcome />
-            </Col>
-          </Row>
-        </Container>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <NavBar />
-        <Container fluid>
-          <Row className="bg-secondary" style={{ height: "100vh" }}>
-            <Col className="d-flex justify-content-center align-content-center">
-              <h1>Please log in to view this content</h1>
-            </Col>
-          </Row>
-        </Container>
-      </>
-    );
-  }
+  return (
+    <>
+      <NavBar />
+      <Container fluid>
+        <Row className="bg-secondary d-flex justify-content-center">
+          <Col lg={8}>
+            <Row>
+              <Col lg={6}>
+                <StockQuote />
+                <ChartComponent />
+              </Col>
+              <Col lg={6}>
+                <StockInfo />
+                <WatchList />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row className="bg-secondary d-flex justify-content-center">
+          <Col>
+            <Welcome />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 export default Main;
--- client/src/components/Main/welcome.js
+++ client/src/components/Main/welcome.js
@@ -4,90 +4,72 @@ import NavBar from "./NavBar";
 import { useSelector } from "react-redux";
 import { authState } from "../../features/auth/authSlice";
 
-const Main = () => {
+const Welcome = () => {
   const auth = useSelector(authState);
-  if (auth.isAuthenticated) {
-    return (
-      <>
-          <Row className="d-flex justify-content-center">
-            <Col className="bg-light p-3" lg={8}>
-              <Row>
-                <Col>
-                  <div className="welcome">
-                    <p>
-                      Welcome to ValuePal, a stock analysis tool gathering data
-                      from{" "}
-                      <a href="https://www.alphavantage.co/">
-                        https://www.alphavantage.co/
-                      </a>
-                    </p>
-                  </div>
-                  <div className="instructions">
-                    <p>
-                      You are already signed in! Enter
-                      a ticker symbol in the search bar at the top right and the
-                      data will populate to the screen. More features will be on
-                      the way, but for now the following options are available
-                      to you:
-                    </p>
-                  </div>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <div className="featurelist">
-                    <ul>
-                      <li>Company Info</li>
-                      <li>Price/Price Range Info</li>
-                      <li>Market Cap</li>
-                      <li>Outstanding Shares</li>
-                      <li>Book Value</li>
-                    </ul>
-                  </div>
-                </Col>
-                <Col>
-                  <div className="featurelist">
-                    <ul>
-                      <li>Earnings per Share</li>
-                      <li>Price/Sales Ratio</li>
-                      <li>Price/Book Ratio</li>
-                      <li>Beta</li>
-                      <li>Adding a stock to your watch list</li>
-                    </ul>
-                  </div>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <div className="helpnotice">
-                    <p>
-                      There is currently a limit on the usage of our API so be
-                      gentle! Please do not search more than once in a minute.
-                      If you see any other bugs feel free to reach out to me at
-                      everett.diuguid@gmail.com. The Github repository can be
-                      found{" "}
-                      <a href="https://github.com/diuguide/valuePal">here.</a>
-                    </p>
-                  </div>
-                </Col>
-              </Row>
+  return (
+    <>
+      <Row className="d-flex justify-content-center">
+        <Col className="bg-light p-3" lg={8}>
+          <Row>
+            <Col>
+              <div className="welcome">
+                <p>
+                  Welcome to ValuePal, a stock analysis tool gathering data from{" "}
+                  <a href="https://www.alphavantage.co/">
+                    https://www.alphavantage.co/
+                  </a>
+                </p>
+              </div>
+              <div className="instructions">
+                <p>
+                  You are already signed in! Enter a ticker symbol in the search
+                  bar at the top right and the data will populate to the screen.
+                  More features will be on the way, but for now the following
+                  options are available to you:
+                </p>
+              </div>
             </Col>
           </Row>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <NavBar />
-        <Container fluid>
-          <Row className="bg-secondary" style={{ height: "100vh" }}>
-            <Col className="d-flex justify-content-center align-content-center">
-              <h1>Please log in to view this content</h1>
+          <Row>
+            <Col>
+              <div className="featurelist">
+                <ul>
+                  <li>Company Info</li>
+                  <li>Price/Price Range Info</li>
+                  <li>Market Cap</li>
+                  <li>Outstanding Shares</li>
+                  <li>Book Value</li>
+                </ul>
+              </div>
+            </Col>
+            <Col>
+              <div className="featurelist">
+                <ul>
+                  <li>Earnings per Share</li>
+                  <li>Price/Sales Ratio</li>
+                  <li>Price/Book Ratio</li>
+                  <li>Beta</li>
+                  <li>Adding a stock to your watch list</li>
+                </ul>
+              </div>
             </Col>
           </Row>
-        </Container>
-      </>
-    );
-  }
+          <Row>
+            <Col>
+              <div className="helpnotice">
+                <p>
+                  There is currently a limit on the usage of our API so be
+                  gentle! Please do not search more than once in a minute. If
+                  you see any other bugs feel free to reach out to me at
+                  everett.diuguid@gmail.com. The Github repository can be found{" "}
+                  <a href="https://github.com/diuguide/valuePal">here.</a>
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  );
 };
-export default Main;
+export default Welcome;
